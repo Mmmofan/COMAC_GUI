02445cbf4d802c7f85220ea1e2267f680eb62a78
Mmmofan <cokespace2@gmail.com>
2019-08-24 22:49:12 +0800

    bug fix

--- UDP_receive.py
+++ UDP_receive.py
@@ -9,27 +9,21 @@ class IGPS_receiver(object):
         self.addr = (self.sever_host, self.sever_port)
         self.data = []
         self.igps_client = socket.socket(family=socket.AF_INET, type=socket.SOCK_DGRAM)
-        # self.igps_client.bind(self.addr)
+        self.igps_client.bind(self.addr)
 
     def read_data(self):
-        self.igps_client.sendto(b'go', self.addr)
-        data,addr = self.igps_client.recvfrom(self.bufsize)
-        # try:
+        data = self.igps_client.recv(self.bufsize)
         dict_data = json.loads(data.decode('utf-8'))
-        self.data += [dict_data]
-        # except:
-        #     self.judge()
+        self.data = dict_data
 
     def close(self):
         self.igps_client.close()
 
     def fliter_data(self):
-        self.output_x=self.data[-1]["coord"]["3"]["X"]
+        self.output_x = self.data[-1]["coord"]["3"]["X"]
         self.output_y = self.data[-1]["coord"]["3"]["y"]
         self.output_z = self.data[-1]["coord"]["3"]["y"]
 
-    # def judge(self):
-    #     pass
 '''
 {"coord":{"3":{"Flags":0,"TransCount":"2.4.","Valid":true,"X":6457.98193359375,"Y":-3672.16162109375,"Z":-9470.173828125}},
 "id":590593573,
@@ -39,7 +33,7 @@ class IGPS_receiver(object):
 if '__name__'=='__main__':
     '''大致操作过程：建立连接，sever开始不断发送数据，read_data一次读取一次数据包，udp性质决定，一次一个完整的包，然后fliter_data对字典
     处理，读取xyz数据，最后可通过close断开连接'''
-    re=IGPS_receiver('127.0.0.1',10000,1024)#建立连接
+    re = IGPS_receiver('127.0.0.1', 10000, 1024)#建立连接
 
 
 
